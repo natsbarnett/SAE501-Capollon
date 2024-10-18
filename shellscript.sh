08259d27471ac486bca0ee65681df22d43d893ac
Nats Barnett <111586282+natsbarnett@users.noreply.github.com>
2024-10-18 08:43:13 +0200

    :/

--- shellscript.sh
+++ shellscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTALLDIR = /var/www/html
+INSTALLDIR = /var/www/html/
 
 # Vérifier si le script est exécuté en tant que superutilisateur
 if [ "$EUID" -ne 0 ]; then
@@ -92,6 +92,58 @@ echo "Modification des droits utilisateurs du dossier /var/www/html/..."
 
 chown -R www-data:www-data /var/www/html/
 chmod -R 775 /var/www/html/
+
+echo "+----------------------------------------------------------------------------------+"
+echo "+---                     Création de l'utilisateur mysql                        ---+"
+echo "+----------------------------------------------------------------------------------+"
+
+
+# Demander le nom d'utilisateur MariaDB ou "quit" pour sortir
+read -p "Entrez le nom d'utilisateur MariaDB à créer (ou tapez 'quit' pour sortir) : " db_username
+
+# Demander le mot de passe pour l'utilisateur MariaDB
+read -sp "Entrez le mot de passe pour $db_username : " db_password
+echo
+
+# Créer l'utilisateur dans MariaDB
+mysql -u root -p -e "CREATE USER '$db_username'@'localhost' IDENTIFIED BY '$db_password';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$db_username'@'localhost';"
+#mysql -u root -p -e "quit;"
+
+# Confirmation de la création
+echo "L'utilisateur MariaDB $db_username a été créé avec succès."
+
+
+
+
+echo "+----------------------------------------------------------------------------------+"
+echo "+---                        Récupération du repository                          ---+"
+echo "+----------------------------------------------------------------------------------+"
+
+echo "Dossier d'installation : $INSTALLDIR"
+cd "$INSTALLDIR"
+
+echo "Récupération de l'archive sur git"
+wget "https://github.com/natsbarnett/SAE501-Capollon/archive/refs/heads/main.zip"
+echo "Fait :D"
+
+echo "Extraction de l'archive :)"
+unzip -q main.zip
+
+cd $INSTALLDIR/SAE501-Capollon-main/
+
+mkdir $INSTALLDIR/capollon
+mv ./* $INSTALLDIR/capollon
+
+cd $INSTALLDIR/capollon
+
+chmod -R 775 /var/www/html
+
+cat "L'installation automatique est terminée, maintenant, installez PHPMyAdmin et installez la base de données. 
+Récupérez aussi les dépendances demandées par Prestashop et changez la taille maximale des upload dans php.ini
+Redémarrez le serveur web avec : systemctl restart apache2"
+
+
 
 # Fin du script
 echo "L'installation du stack LAMP avec MariaDB est terminée !"
