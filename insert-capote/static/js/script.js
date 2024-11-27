8933625ae17c82bb0d22c0d7856a7bac95134b6f
Belugwatt <114382509+Belugwatt@users.noreply.github.com>
2024-11-27 08:19:47 +0100

    début interface

--- insert-capote/static/js/script.js
+++ insert-capote/static/js/script.js
@@ -1,4 +1,5 @@
 // Sélection des éléments
+const fileInput = document.getElementById("img");
 const nomInput = document.getElementById("nom");
 const prixInput = document.getElementById("prix");
 const descInput = document.getElementById("desc");
@@ -7,6 +8,7 @@ const addButton = document.getElementById("addProduct");
 // Fonction pour vérifier si tous les champs sont remplis
 function checkFields() {
     const isFilled =
+        fileInput.files.length = 1 &&
         nomInput.value.trim() !== "" &&
         prixInput.value.trim() !== "" &&
         descInput.value.trim() !== "";
@@ -19,6 +21,21 @@ function checkFields() {
 }
 
 // Ajouter des événements d'écoute sur les champs
+fileInput.addEventListener("input", checkFields);
 nomInput.addEventListener("input", checkFields);
 prixInput.addEventListener("input", checkFields);
 descInput.addEventListener("input", checkFields);
+
+addButton.addEventListener("click",()=>{
+    validator();
+})
+
+function validator() {
+    if (button.classList.contains('desactive')) {
+        if(isFilled){
+
+        }else {
+            alert("Veuillez remplire tous les champs");
+        }
+    }
+}
